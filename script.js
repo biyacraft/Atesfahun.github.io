010f564bfde6460d276fbed1fb27ce4350b2d726
abbyyT <abitesfahunn@gmail.com>
2023-09-08 01:38:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -53,9 +53,27 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 
+
+
 };
+ScrollReveal({
+
+    //reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 500
+});
 
 
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .portfolio-box, .contact form', { origin: 'top' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 
-
+const typed = new Typed('.multiple-text', {
+    strings: ['Computer Engineer', 'Frontend Developer', 'Content Creator'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
